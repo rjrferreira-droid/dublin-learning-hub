9c6b254d2ad7a3646d4ccde8bf193a80da4136b9
rjrferreira-droid <rjrferreira@hotmail.com>
2026-09-04 08:07:27 -0300

    Mount V2 cost center globally

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { CostCenterPanel } from './components/CostCenterPanel';
 import './styles.css';
 import './british-premium.css';
 import './premium-audio.css';
 import './professor.css';
 import './learning-intelligence.css';
+import './cost-center.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <CostCenterPanel />
     </BrowserRouter>
   </React.StrictMode>,
 );
