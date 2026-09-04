49dd43dfdac0aa32714807cfef14bb5abb306441
rjrferreira-droid <rjrferreira@hotmail.com>
2026-09-04 10:34:01 -0300

    Gate Learning Hub V2 behind Supabase authentication

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { AuthGate } from './components/AuthGate';
 import { CostCenterPanel } from './components/CostCenterPanel';
 import './styles.css';
 import './british-premium.css';
@@ -9,12 +10,15 @@ import './premium-audio.css';
 import './professor.css';
 import './learning-intelligence.css';
 import './cost-center.css';
+import './auth.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-      <CostCenterPanel />
+      <AuthGate>
+        <App />
+        <CostCenterPanel />
+      </AuthGate>
     </BrowserRouter>
   </React.StrictMode>,
 );
